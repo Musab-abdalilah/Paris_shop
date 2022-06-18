6393bfcb931019d9cab8d3a9e38e1fe0f5d36af8
Musab-Abdullah <musababdo7790@gmail.com>
2022-06-18 00:09:35 -0700

    Simple shopping site

---
+++ javas.js
@@ -0,0 +1,58 @@
+
+
+// ----------- for toggle menu -----------------------------------
+var MenuItems=document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight="0px";
+function menutoggle(){
+    if (MenuItems.style.maxHeight=="0px"){
+        MenuItems.style.maxHeight="200px";
+    }
+    else{
+        MenuItems.style.maxHeight="0px";
+    }
+}
+
+// ---------------------------------------------------------------
+
+
+
+
+// --------------_ toggle form _----------------------------------
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+
+function register(){
+    RegForm.style.transform="translateX(0px)";
+    LoginForm.style.transform="translateX(0px)";
+    Indicator.style.transform="translateX(-122px)";
+}
+function login(){
+    RegForm.style.transform="translateX(-300px)";
+    LoginForm.style.transform="translateX(-300px)";
+    Indicator.style.transform="translateX(0px)";
+}
+// -----------------------------------------------------------
+
+
+
+
+// -------------product galery-------------------------------
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick=function(){
+    ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onclick=function(){
+    ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onclick=function(){
+    ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onclick=function(){
+    ProductImg.src = SmallImg[3].src;
+}
+// ----------------------------------------------------------
